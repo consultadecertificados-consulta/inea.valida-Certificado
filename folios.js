9e92bf08c0907b91b0e9511516d05293c2a7d515
consultadecertificados-consulta <consultadecertificados@gmail.com>
2026-01-13 13:57:32 -0600

    Update folios.js

--- folios.js
+++ folios.js
@@ -30,7 +30,7 @@ const baseDatos = {
    "9da9226f-6e37-41cc-81f6-23cb903e6593": { nombre: "LINDA IVETH BERNAL ARMAS", nivel: "SECUNDARIA", promedio: "8.5", conclusion: "2025-11-15", emision: "2025-11-20", lugar: "CUAUTITLAN IZCALLI, EDO MEX", id: "3541258494" },
     "4da9226f-6e17-41cc-81f6-23cb903e6593": { nombre: "CELSO JESUS SANCHEZ MUÑIS", nivel: "SECUNDARIA", promedio: "8.6", conclusion: "2025-11-15", emision: "2025-11-20", lugar: "MONTERREY , NUEVO LEON", id: "3642258605" },
     "4da9226f-6e17-41cc-81g6-23cb903e6593": { nombre: "FLOR GRISELDA DUBLE CARRILLO", nivel: "SECUNDARIA", promedio: "8.5", conclusion: "2025-11-15", emision: "2025-11-20", lugar: "CHIHUAHUA, CHIUAHUA", id: "3743258716" },
-    "folio-inf-o7": { nombre: "RICARDO MARÍN PAZ", nivel: "SECUNDARIA", promedio: "8.3", conclusion: "2024-11-01", emision: "2024-11-06", lugar: "LEÓN, GTO", id: "3844258827" },
+     "4da9226f-6e17-41ec-81g6-23cb903e6593": { nombre: "YARELI BENICE DUBLE CARRILLO", nivel: "SECUNDARIA", promedio: "8.5", conclusion: "2025-11-15", emision: "2025-11-20", lugar: "CHIHUAHUA, CHIHUAHUA", id: "3844258827" },
     "folio-inf-28": { nombre: "ESTHER NOEMI LOPEZ", nivel: "SECUNDARIA", promedio: "8.7", conclusion: "2024-12-30", emision: "2025-01-05", lugar: "GUANAJUATO, GTO", id: "3945258928" },
     "folio-inf-29": { nombre: "ALBERTO DIAZ G.", nivel: "PRIMARIA", promedio: "7.6", conclusion: "2024-09-22", emision: "2024-09-27", lugar: "QUERÉTARO, QRO", id: "4046259039" },
     "folio-inf-30": { nombre: "SANDRA LUZ MARTINEZ", nivel: "SECUNDARIA", promedio: "9.1", conclusion: "2024-10-28", emision: "2024-11-02", lugar: "LEON, GTO", id: "4147259140" },
@@ -66,6 +66,7 @@ const baseDatos = {
     "folio-inf-60": { nombre: "LORENA HERRERA P.", nivel: "SECUNDARIA", promedio: "8.9", conclusion: "2025-03-25", emision: "2025-03-30", lugar: "ACAPULCO, GRO", id: "7177262470" }
 
 };
+
 
 
 
