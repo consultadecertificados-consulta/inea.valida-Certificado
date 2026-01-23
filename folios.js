536a60340a35402395607bb2d0a9f3bb91f632aa
consultadecertificados-consulta <consultadecertificados@gmail.com>
2026-01-23 10:24:45 -0600

    Update folios.js

--- folios.js
+++ folios.js
@@ -51,7 +51,7 @@ const baseDatos = {
     "1da9226f-6e07-41cc-81f6-888b903e6593": { nombre: "ANTONIA CRUZ RAMOS", nivel: "SECUNDARIA", promedio: "8.5", conclusion: "2025-09-20", emision: "2025-09-25", lugar: "REYNOSA, TAMAULIPAS", id: "5662260805" },
      "6da9226f-6e07-41cc-81f6-23cb903e6666": { nombre: "RAMIRO OCHOA BRICEÑO", nivel: "SECUNDARIA", promedio: "8.5", conclusion: "2025-11-15", emision: "2025-11-20", lugar: "REYNOSA, TAMAULIPAS", id: "5763260916" },
    "9da92666-6e07-41cc-81f6-23cb903e6593": { nombre: "VICTORIA NAYELI JULIAN LOPEZ", nivel: "SECUNDARIA", promedio: "8.5", conclusion: "2025-12-15", emision: "2025-11-20", lugar: "TLAQUEPAQUE, JALISCO", id: "5864261027" },
-   "0888226f-6e07-41cc-81f6-23cb903e6593": { nombre: "PEDRO JOSE ROJAS CHAVEZ", nivel: "SECUNDARIA", promedio: "8.5", conclusion: "2025-11-15", emision: "2025-11-20", lugar: "VERACRUZ, VERACRUZ", id: "5965261138" },
+   "0888226f-6e07-41cc-81f6-23cb903e6593": { nombre: "------------------------", nivel: "SECUNDARIA", promedio: "8.5", conclusion: "2025-11-15", emision: "2025-11-20", lugar: "VERACRUZ, VERACRUZ", id: "5965261138" },
     "9da9226f-6e07-41cc-81f6-999b903e6593": { nombre: "BERENICE GONZALEZ LAZARO", nivel: "SECUNDARIA", promedio: "8.5", conclusion: "2025-11-15", emision: "2025-11-20", lugar: "SAN LUIS POTOSI", id: "6066261249" },
     "folio-inf-50": { nombre: "YOLANDA REYES GIL", nivel: "PRIMARIA", promedio: "8.8", conclusion: "2025-02-25", emision: "2025-03-01", lugar: "LA PAZ, BCS", id: "6167261360" },
     "folio-inf-51": { nombre: "RAMON VALDEZ SOTO", nivel: "SECUNDARIA", promedio: "7.8", conclusion: "2025-02-28", emision: "2025-03-04", lugar: "DURANGO, DGO", id: "6268261471" },
@@ -66,6 +66,7 @@ const baseDatos = {
     "folio-inf-60": { nombre: "LORENA HERRERA P.", nivel: "SECUNDARIA", promedio: "8.9", conclusion: "2025-03-25", emision: "2025-03-30", lugar: "ACAPULCO, GRO", id: "7177262470" }
 
 };
+
 
 
 
